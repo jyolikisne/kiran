de85aab2d142a3eb131609e27f3f169720389202
PC <PC@192.168.0.4>
2018-12-11 11:52:18 +0530

    adsf

--- jupitor/src/eroupa/moon.java
+++ jupitor/src/eroupa/moon.java
@@ -3,11 +3,7 @@ package eroupa;
 public class moon {
 
 	public static void main(String[] args) {
-		System.out.println("hai jyothsna");
-<<<<<<< HEAD
-		
-=======
->>>>>>> branch 'master' of https://github.com/jyolikisne/kiran
+		System.out.println("hai jyolikisne");
 
 	}
 
